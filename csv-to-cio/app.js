9389914e282b0a8dc50104eba0f1216fb33fa8b8
Sergii Tkachenko <hi@sergii.org>
2017-05-23 15:59:19 -0400

    getNorthstarUser()

--- csv-to-cio/app.js
+++ csv-to-cio/app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const fs = require('fs');
 const yargs = require('yargs');
 const csv = require('csv-parser')
+require('isomorphic-fetch');
 
 // ------- Args parse ----------------------------------------------------------
 
@@ -22,7 +23,36 @@ const argv = yargs
 // ------- App bootstrap -------------------------------------------------------
 
 const processRow = async (row) => {
-  console.dir(row.id, { colors: true, showHidden: true });
+  let result;
+  try {
+    result = await getNorthstarUser(row.id);
+  } catch (e) {
+    console.error(`Can't process ${row.id}: ${e}`);
+    return;
+  }
+
+  console.dir(result, { colors: true, showHidden: true });
+}
+
+const getNorthstarUser = async (id) => {
+  let result;
+  try {
+    const response = await fetch(
+      `${process.env.DS_NORTHSTAR_API_BASEURI}/users/id/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.DS_NORTHSTAR_API_KEY}`
+        }
+      }
+    );
+    result = await response.json();
+  } catch (e) {
+    throw new Error(`Northstar error: ${e}`);
+  }
+  if (result.error) {
+    throw new Error(`Northstar response: ${result.error.message}`);
+  }
+  return result.data;
 }
 
 fs.createReadStream(argv.file)
