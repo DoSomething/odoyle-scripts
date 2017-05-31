c25bb16f14eedd80df569d03781b89ac1d33864c
Rafa <rapala61@gmail.com>
2017-05-31 01:46:47 -0400

    added test config for gambit

--- gambit-stuck-submissions-import/config/lib/gambit.js
+++ gambit-stuck-submissions-import/config/lib/gambit.js
@@ -1,9 +1,14 @@
 const configVars = {};
 
-if (process.env.NODE_ENV === 'production') {
+configVars.environment = process.env.NODE_ENV;
+
+if (configVars.environment === 'production') {
   configVars.baseURL = process.env.GAMBIT_API_BASE_URL;
   configVars.gambitAPIKey = process.env.GAMBIT_API_KEY;
-}else {
+} else if (configVars.environment === 'test') {
+  configVars.baseURL = 'http://localhost:9001';
+  configVars.gambitAPIKey = 'totallysecret';
+} else {
   configVars.baseURL = 'http://localhost:5000/v1';
   configVars.gambitAPIKey = 'totallysecret';
 }
