f3b77da601d6f13ff6ece62ec2e89fcd0cf83f53
Rafa <rapala61@gmail.com>
2017-05-31 00:26:50 -0400

    Implemented the post functionality using batch processing and events in lib/gambit

--- gambit-stuck-submissions-import/lib/gambit.js
+++ gambit-stuck-submissions-import/lib/gambit.js
@@ -1,21 +1,69 @@
 const axios = require('axios');
 const underscore = require('underscore');
+const logger = require('winston');
+const eventEmitter = require('events');
+const assert = require('assert');
+
+const config = require('../config/lib/gambit');
+const BatchProcessor = require('./batchProcessor');
 
 const defaults = {
-  baseURL: process.env.GAMBIT_API_BASE_URL,
+  baseURL: config.baseURL,
   timeout : 29000,
   headers: {
-    'x-gambit-api-key': process.env.GAMBIT_API_KEY,
-  }
+    'x-gambit-api-key': config.gambitAPIKey,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
 };
 
-class GambitService {
-  constructor(options) {
-    this.config = underscore.extend({}, defaults, options);
-    this.client = axios.create(this.config);
+class GambitService extends eventEmitter {
+  constructor(options = {}, batchProcessor) {
+    super();
+    this.options = underscore.extend({}, defaults, options.http || {});
+    this.client = axios.create(this.options);
+    this.batchProcessor = batchProcessor ? batchProcessor : new BatchProcessor();
+    this.errors = [];
+    this.setupEventHandlers();
   }
-  post(endpoint, data = {}, options = {}) {
-    return this.client.post(endpoint, data, options);
+  setupEventHandlers(){
+    if (!this.batchProcessor instanceof eventEmitter) {
+      throw new Error('This batchProcessor doesn\'t support events.');
+    }
+    this.batchProcessor.on('done', (processed) => {
+      this.emit('done', this.errors, processed);
+    });
+
+    this.on('sent', (response) => {
+      logger.info(`Successfully sent request. Status: ${response.status}. Data: ${JSON.stringify(response.data)}`);
+    });
+
+    this.on('error', (error) => {
+      logger.error(`Error processing a request: ${error}`);
+    });
+  }
+  enqueueRequest(request) {
+    this.batchProcessor.enqueue({
+      args: request,
+      cb: this.send.bind(this),
+    });
+  }
+  send({ method = 'post', endpoint, data = {}, options = {} }) {
+    if (method.toLowerCase() === 'post') {
+      return this.client.post(endpoint, data, options)
+        .then((response) => {
+          this.emit('sent', response);
+        })
+        .catch((error) => {
+          this.emit('error', error);
+          this.errors.push({
+            error,
+            request: { method, endpoint, data, options }
+          });
+        });
+    }
+  }
+  startBatchProcess() {
+    this.batchProcessor.start();
   }
 }
 
