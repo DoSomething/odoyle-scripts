af6c24d25d3c36da01de8d3d51a9bce1f3c31e30
Rafa <rapala61@gmail.com>
2017-10-09 11:51:19 -0500

    fixed exposed credentials

--- load-tester/load-tests/broadcasts/vucode/config.js
+++ load-tester/load-tests/broadcasts/vucode/config.js
@@ -6,14 +6,14 @@ export default {
   broadcastSettingsUrl: `${__ENV.broadcastSettingsBaseUrl}/${__ENV.broadcastId}`,
   statusCallbackRequestMock: {
     // We won't use them to avoid confusion. They're included for legacy support according to Twilio
-    // SmsSid: "SM209da8432cb84d549916f703ef9eb3e4",
+    // SmsSid: "SMxxx",
     // SmsStatus: "sent",
     MessageStatus: "sent",
-    To: "+19144107389",
-    MessagingServiceSid: "MGd8b9511cf6b463b673a6ce660a08ef3d",
-    MessageSid: "SM209da8432cb84d549916f703ef9eb3e4",
-    AccountSid: "AC54a6683e7e5a2232aa9679003e84a685",
-    From: "+13056152849",
+    To: "+555555555",
+    MessagingServiceSid: "MGxx",
+    MessageSid: "SM2xx",
+    AccountSid: "ACxx",
+    From: "+15555555555",
     ApiVersion: "2010-04-01"
   },
 
