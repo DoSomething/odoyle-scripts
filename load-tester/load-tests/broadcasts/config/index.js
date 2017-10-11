50aa6523b2bbd6b5fd9997bf77446cad657d3025
Rafa <rapala61@gmail.com>
2017-10-11 10:11:45 -0500

    better naming for broadcastId env variable

--- load-tester/load-tests/broadcasts/config/index.js
+++ load-tester/load-tests/broadcasts/config/index.js
@@ -5,7 +5,7 @@ const config = {
 };
 
 const broadcastSettingsBaseUrl = process.env.DS_CONVERSATIONS_BROADCASTS_SETTINGS_URL || 'http://puppet:totallysecret@localhost:5100/api/v1/broadcast-settings';
-const broadcastId = process.env.DS_CONVERSATIONS_BROADCAST_SETTINGS_ID || 'tacosfest';
+const broadcastId = process.env.DS_CONVERSATIONS_BROADCAST_ID || 'tacosfest';
 
 config.commandEnvVariables = {
   broadcastSettingsBaseUrl,
