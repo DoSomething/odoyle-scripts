0bfd33850ce0f9a3f4571a0bf2b2ad68f6c23a8c
Aaron Schachter <aaronschachter@gmail.com>
2017-10-31 14:47:41 -0700

    Gets basic upsert working

--- tgm-conversations/index.js
+++ tgm-conversations/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const fs = require('fs');
 const logger = require('winston');
 const neatCsv = require('neat-csv');
+const args = require('yargs').argv;
 const helpers = require('./lib/helpers');
 const config = require('./config');
 
@@ -13,36 +14,58 @@ async function getDB() {
   return db;
 }
 
-async function init() {
+async function upsertRows(rows, campaignId, topic = 'random') {
   const db = await getDB();
-  try {
-    const conversations = await db.collection('conversations').find().limit(5).toArray();
-    conversations.forEach((conversation, index) => {
-      logger.info(`${index} conversationId:${conversation._id}`);
-    });
-  } catch(error) {
-    logger.error(error);
+  const result = {
+    processed: 0,
+    errors: [],
+  };
+
+  for (let i = 0; i < rows.length; i++) {
+    result.processed++;
+    try {
+      const row = rows[i];
+      const platformUserId = helpers.formatMobileNumber(row.phone_number);
+      const data = {
+        platform: 'sms',
+        platformUserId,
+        campaignId,
+        topic,
+        importSource: 'import',
+      };
+      const query = { platformUserId  };
+      await db.collection('conversations').update(query, data, { upsert: true });
+      logger.info('success', { data });
+    } catch(error) {
+      result.errors.push(error);
+      logger.error(error);
+    }
   }
+
+  return result;
 }
 
 // ------- Main function --------------------------------------------------------
 
-const importConversations = async (stream) => {
+if (!args.file || !args.campaign) {
+  console.log('Missing file or campaign args');
+  process.exit(0);
+}
+
+const main = async (stream) => {
   const data = await neatCsv(stream, {
     separator: ',',
   });
-
-  const rowCount = data.length;
-  data.forEach((row, i) => {
-    const mobile = helpers.formatMobileNumber(row.phone_number);
-    logger.info('Importing', { i, mobile });
-  });
-
-  logger.info(`Processed ${rowCount} rows.`);
+  const result = await upsertRows(data, args.campaign);
+  const errors = result.errors;
+  logger.info(`Processed ${result.processed} rows.`);
+  if (errors.length) {
+    logger.info(`Found ${errors.length} errors:`, errors);
+  }
+  process.exit(0);
 }
 
-const path = '../../TGM/7554.csv'
-const csvFile = fs.createReadStream(path)
+const csvFile = fs.createReadStream(args.file)
   .on('error', (e) => winston.error(`Parse error | ${e}`));
 
-importConversations(csvFile);
+main(csvFile);
